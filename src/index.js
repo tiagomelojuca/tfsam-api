946dc6bf0224ff0dcad5526c26e4322f8d2a2f66
Tiago Melo Juca <tiagomelojuca@gmail.com>
2020-06-09 00:33:10 -0300

    Use CORS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const AccountController = require('./controllers/AccountController');
 const CharacterController = require('./controllers/CharacterController');
 
 const server = express();
 server.use(express.json());
+server.use(cors( {
+    origin: 'http://yourdomain.com',
+    optionsSuccessStatus: 200
+} ));
+
 const accountController = new AccountController();
 const characterController = new CharacterController();
 
